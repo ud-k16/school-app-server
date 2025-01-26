645b5b127ce51b1a705d0c4cf62ecd6a0f486dc9
uma <uma@tigeen.ch>
2025-01-26 15:32:50 +0530

    new file for mockdata

---
+++ src/mockdata.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    id: 1,
+    name: "Geography",
+    teacher_name: "ManiMozhi",
+  },
+  {
+    id: 2,
+    name: "History",
+    teacher_name: "seetha",
+  },
+  {
+    id: 3,
+    name: "Tamil",
+    teacher_name: "Tamil Ayya",
+  },
+];
+
+const timetable = [
+  {
+    day: "monday",
+    time_slot: [],
+  },
+];
