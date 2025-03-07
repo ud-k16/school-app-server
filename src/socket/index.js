a3b0b12737d7f1cc36151290dd92c260f22c0afb
uma <uma@tigeen.ch>
2025-03-07 19:24:07 +0530

    object logging removed

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,11 +1,11 @@
 var WebSocket = require("ws");
-var socketInstance = new WebSocket.Server({ port: 5004 });
+var socketInstance = new WebSocket.WebSocketServer({ port: 5004 });
 
 socketInstance &&
   console.log("web socket listening on port ", socketInstance.address().port);
 
 socketInstance.on("connection", (ws) => {
-  console.log("Client connected", ws);
+  console.log("Client connected");
   ws.on("message", (message) => {
     console.log("received: %s", message);
   });
